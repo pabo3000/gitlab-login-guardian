d42ac3e63c7feccc75442690309ace82c06af26c
Pavel Bogdanovic <pb@prontonet.eu>
2025-07-24 14:21:52 +0200

    typo

--- gitlab_login_guardian/main.py
+++ gitlab_login_guardian/main.py
@@ -4,7 +4,7 @@ import time
 from core import GitlabLoginGuardian
 
 guardian = GitlabLoginGuardian(
-    log_file="/var/log/gitlab-login-ban.log",
+    log_file="/var/log/gitlab/gitlab-rails/production_json.log",
     ban_log="/var/log/gitlab-login-ban.log",
     blocklist_file="/etc/gitlab/nginx/custom/ip_blocklist.conf",
     meta_file="/etc/gitlab/nginx/custom/ip_blocklist_meta.json",
